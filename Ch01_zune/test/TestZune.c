8dcbfa6a49aa2c104598f862252251e76e5aa706
Junggu Lee <hyde1117@naver.com>
2014-04-19 07:30:36 +0900

    executable version

---
+++ Ch01_zune/test/TestZune.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(zune);
+
+TEST_SETUP(zune)
+{
+
+}
+
+TEST_TEAR_DOWN(zune)
+{
+
+}
+
+TEST(zune, init)
+{
+	TEST_FAIL_MESSAGE("Start here");
+}
