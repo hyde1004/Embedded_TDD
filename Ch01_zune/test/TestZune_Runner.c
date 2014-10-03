a947dce772a11251603382409bec0bd258947597
Junggu Lee <junggu.lee@lge.com>
2014-10-03 11:18:18 +0900

    Delete all

--- Ch01_zune/test/TestZune_Runner.c
+++
@@ -1,8 +0,0 @@
-#include "unity.h"
-#include "unity_fixture.h"
-
-TEST_GROUP_RUNNER(zune)
-{
-	RUN_TEST_CASE(zune, test_daysSince1980ForYear);
-	RUN_TEST_CASE(zune, 2008_12_31_last_day_of_leap_year);
-}
