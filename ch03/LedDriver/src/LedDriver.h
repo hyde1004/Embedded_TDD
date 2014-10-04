b14492013ef8e14c662d88f572649cbbe75f99f5
Junggu Lee <hyde1117@naver.com>
2014-10-04 16:29:48 +0900

    p50

--- ch03/LedDriver/src/LedDriver.h
+++ ch03/LedDriver/src/LedDriver.h
@@ -1,9 +1,9 @@
-extern "C"
-{
 #include <stdint.h>
 
-
+extern "C"
+{
 void LedDriver_Create(uint16_t * address);
 void LedDriver_Destroy(void);
 void LedDriver_TurnOn(int ledNumber);
+void LedDriver_TurnOff(int ledNumber);
 }
