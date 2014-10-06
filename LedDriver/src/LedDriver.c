a8f6f3b05ddfa2acc09d89de16188c823b1ca617
Junggu Lee <junggu.lee@lge.com>
2014-10-06 16:37:02 +0900

    p63

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -12,9 +12,14 @@ void LedDriver_Destory(void)
 {
 }
 
+static uint16_t convertLedNumberToBit(int ledNumber)
+{
+	return 1 << (ledNumber - 1);
+}
+
 void LedDriver_TurnOn(int ledNumber)
 {
-	*ledAddress |= 1 << (ledNumber - 1);
+	*ledAddress |= convertLedNumberToBit(ledNumber);
 }
 
 void LedDriver_TurnOff(int ledNumber)
