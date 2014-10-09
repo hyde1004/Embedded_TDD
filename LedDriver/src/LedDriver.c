ef4de1be218dec8a5d0d067665f9211b0f8e6e52
Junggu Lee <hyde1117@naver.com>
2014-10-09 12:06:53 +0900

    중복코드 분리

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -5,37 +5,41 @@ enum { ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON };
 static uint16_t * ledAddress;
 static uint16_t ledsImage;
 
+static uint16_t convertLedNumberToBit(int ledNumber)
+{
+	return 1 << (ledNumber - 1);
+}
+static void updateHardware(void)
+{
+	*ledAddress = ledsImage;
+}
+
 void LedDriver_Create(uint16_t * address)
 {
 	ledAddress = address;
 	ledsImage = ALL_LEDS_OFF;
-	*ledAddress = ledsImage;
+	updateHardware();
 }
 
 void LedDriver_Destory(void)
 {
 }
 
-static uint16_t convertLedNumberToBit(int ledNumber)
-{
-	return 1 << (ledNumber - 1);
-}
-
 void LedDriver_TurnOn(int ledNumber)
 {
 	ledsImage |= convertLedNumberToBit(ledNumber);
-	*ledAddress = ledsImage;
+	updateHardware();
 }
 
 
 void LedDriver_TurnOff(int ledNumber)
 {
 	ledsImage &= ~(convertLedNumberToBit(ledNumber));
-	*ledAddress = ledsImage;
+	updateHardware();
 }
 
 void LedDriver_TurnAllOn(void)
 {
 	ledsImage = ALL_LEDS_ON;
-	*ledAddress = ledsImage;
+	updateHardware();
 }
